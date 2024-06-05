54b1cc8f52bcd991a07ddc22959cba5e988dafd6
Mirodil Fayzullayev <odilfayz22@gmail.com>
2024-06-05 17:53:46 +0500

    Update to-do-context.tsx

--- src/store/to-do-context.tsx
+++ src/store/to-do-context.tsx
@@ -35,7 +35,7 @@ export function TodoProvider({ children }: TodoProviderProps) {
         toast.error(data.message);
       }
     } catch (err: any) {
-      toast.error(err.message + 7);
+      toast.error(err.message);
     }
   }
 
